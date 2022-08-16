1028d942240fa0735fc9a1d3257895d74540eedc
juraantoniv <juraantoniv@gmail.com>
2022-08-16 22:52:38 +0300

    ok

--- 8/js.js
+++ 8/js.js
@@ -30,7 +30,11 @@ function Car(nameProd, year, maxSpeed, value) {
         console.log(`Рік змінено на ${this.year}`);
 
     }
-
+    // this.addDriver = function (newDriver){
+    // }
+    //  this.addDriver = function(owner) {
+    //      this.['newDr']=owner
+    // }
 }
 
 
@@ -45,34 +49,62 @@ driver1.drive()
 driver1.info()
 driver1.incMax(100)
 driver1.changeYear(2005)
-console.log(driver1);
-
-
-//
-// class Customer extends Car {
-//     constructor(nameProd, year, maxSpeed, value,newSped) {
-//         super(nameProd, year, maxSpeed, value);
-//         this.newSped = newSped;
-//         // this.increeMax=function (newSped){
-//         //     maxSpeed+=newSped
-//         //     console.log(`${maxSpeed}`);
-//         //
-//         // }
-//     }
-// }
+// driver1.addDriver('New driver')
+console.log(driver1)
 
 
 
-//
-// function User(name, age, status, wifeName, wifeAge) {
-//     this.name = name;
-//     this.age = age;
-//     this.status = status;
-//     this.wife = {name: wifeName, age: wifeAge}
-//     this.greeting = function () {
-//         console.log('hi');
-//     };
-// }
-//
-// let user = new User('asd', 123, true, 'anna', 28);
-// console.log(user);
+// - Створити функцію конструктор для об'єктів User з полями id, name, surname , email, phone
+// створити пустий масив, наповнити його 10 об'єктами new User(....)
+
+function User(id, name, surname , email, phone) {
+       this.id = id;
+       this.name = name;
+       this.surname= surname;
+       this.email= email;
+       this.phone= phone;
+   }
+
+
+   let user1 = new User(25,'ivan','ivanov','vdss@gmail.com',+380977863287)
+let user2 = new User(11, 'andrii', 'ivanov', 'adsdsd@gmail.com', +380977863557)
+let user3 = new User(17, 'vitalii', 'ivanov', 'adsdsd@gmail.com', +380977863557)
+let user4 = new User(18, 'andrii', 'ivanov', 'adsdsd@gmail.com', +380977863557)
+let user5 = new User(5, 'stepan', 'ivanov', 'adsdsd@gmail.com', +380977855557)
+let user6 = new User(15, 'petro', 'ivanov', 'adsdsd@gmail.com', +380977863557)
+let user7 = new User(6, 'vasya', 'ivanov', 'adsdsd@gmail.com', +380987863897)
+
+let users =[user1,user2,user3,user4,user5,user6,user7]
+
+console.log(users);
+
+//- Взяти масив з  User[] з попереднього завдання, та відфільтрувати , залишивши тільки об'єкти з парними id (filter)
+
+console.log(users.filter(value => value.id%2===0))
+
+//- Взяти масив з  User[] з попереднього завдання, та відсортувати його по id. по зростанню (sort)
+
+console.log(users.sort((a, b) => a.id-b.id))
+
+// - створити класс для об'єктів Client з полями id, name, surname , email, phone, order (поле є масивом зі списком товарів)
+// створити пустий масив, наповнити його 10 об'єктами Client
+
+class Client {
+    constructor(id, name, surname , email, phone,...order) {
+        this.id = id;
+        this.name = name;
+        this.surname= surname;
+        this.email= email;
+        this.phone= phone;
+        this.order= order;
+    }
+}
+
+let client1 = Client(25,'Vasya','Pupkin', 'dsgss@gmail.com', +380977863872, 'jhjhggh,fsfsdf,sfsfs',fsfsdfsd,55555,hffgf')
+let client2 = Client(20,'Andrii','Pupkin', 'dsgss@gmail.com', +380977863872, 'dgsdgs,fsfsdf,fsfsfs,fsfsdfsd')
+let client3 = Client(15,'Vasya','Pupkin', 'dsgss@gmail.com', +380977863872, 'dgsdgs,fsfsdf,fsfsdfsd,2525252')
+let client4 = Client(10,'Vasya','Pupkin', 'dsgss@gmail.com', +380977863872, 'dgsdgs,fsfsdf,fsfsdfsd')
+let client5 = Client(10,'Vasya','Pupkin', 'dsgss@gmail.com', +380977863872, 'dgsdgs,fsfsdf,fsfsdfsd')
+let client6 = Client(10,'Vasya','Pupkin', 'dsgss@gmail.com', +380977863872, 'dgsdgs,fsfsdf,fsfsdfsd')
+let client7 = Client(10,'Vasya','Pupkin', 'dsgss@gmail.com', +380977863872, 'dgsdgs,fsfsdf,fsfsdfsd')
+let client8 = Client(10,'Vasya','Pupkin', 'dsgss@gmail.com', +380977863872, 'dgsdgs,fsfsdf,fsfsdfsd')
