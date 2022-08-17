5ce7a0f5fb479353562159b22d6a282b88e2f9ea
juraantoniv <juraantoniv@gmail.com>
2022-08-17 21:52:51 +0300

    ok

--- 8/add/js.js
+++ 8/add/js.js
@@ -1,71 +1,55 @@
 
 class User{
-    constructor ( name,  username, email) {
+    constructor ( name,  username, email,street,suite,city,zipcode,lat,lng) {
         this.name =name
         this.username = username
         this.email=email
+        this.adres ={
+            street: street,
+            suite: suite,
+            city: city,
+            zipcode: zipcode,
+            geo: {
+                lat: lat,
+                lng: lng
+            }
+        }
     }
 
 }
-class Address{
-    constructor ( street,  suite,   city,zipcode,) {
-        this.street =street
-        this.suite = suite
-        this.city=city
-        this.zipcode = zipcode
-    }
-}
-
-class Geo {
-    constructor(lat, lng) {
-        this.lat = lat
-        this.lng = lng
-    }
-}
-
 
 class Phone {
-    constructor(phone,  website) {
+    constructor(phone,  website,name,catchPhrase,bs) {
         this.phone = phone
         this.website = website
+        this.company ={
+            name: name,
+            catchPhrase:catchPhrase,
+            bs:bs
+        }
     }
 }
 
-class  Company{
-    constructor(name,catchPhrase,bs) {
-        this.name =name
-        this.catchPhrase= catchPhrase
-        this.bs =bs
-    }
-}
+
+let user = new User('Sacha','saaaaa','fdfd@gmail.com','gdgdgdg','dgdgdgdgd','dgdgdgdg','dgdgdgdgd','22,1222','22,55888')
+let pho = new Phone(78952566,'www.empty.com','fgdfgdg','sfsdfsfsf','fsfsdfsfsfsd')
 
 
-let user = new User('Sacha','saaaaa','fdfd@gmail.com')
-let adr = new Address('nnnnn', 'fsfsfs str','Lviv','79000')
-let geO = new Geo('55,66666','22,65584')
-let pho = new Phone(78952566,'www.empty.com')
-let company = new  Company('Romaguera-Crona','Multi-layered client-server neural-ne', 'harness real-time e-markets')
+
+let all= [ user,pho]
+console.log(all)
 
 
-let arrAll = [user,{adr,geO},{pho,company}]
 
-let arrAll1 = new User('Sacha','saaaaa','fdfd@gmail.com',
-    new Address('nnnnn', 'fsfsfs str','Lviv','79000',
-        new Geo('55,66666','22,65584'),
-        new Phone(78952566,'www.empty.com',
-            new  Company('Romaguera-Crona','Multi-layered client-server neural-ne', 'harness real-time e-markets'))))
 
-console.log(arrAll)
-console.log(arrAll1)
-
-//--------------------------------------------------2 незнаю як користуватись http://htmlbook.ru/)
-
-document.write(`<div class="box">`)
-
-document.write(`<h4 class="text">${user.name}</h4>`)
-document.write(`<p class="text" >Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>`)
-document.write(`<div class="text" >Phone : ${pho.phone} | ${pho.website}</div>`)
-document.write(`<div class="text" >Phone : ${adr.city} | ${adr.zipcode}</div>`)
-document.write(`<div class="text" >GPS:  ${geO.lat} | ${geO.lng}</div>`)
-
-document.write(`</div>`)
+// //--------------------------------------------------2 незнаю як користуватись http://htmlbook.ru/)
+//
+// document.write(`<div class="box">`)
+//
+// document.write(`<h4 class="text">${user.name}</h4>`)
+// document.write(`<p class="text" >Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>`)
+// document.write(`<div class="text" >Phone : ${pho.phone} | ${pho.website}</div>`)
+// document.write(`<div class="text" >Phone : ${adr.city} | ${adr.zipcode}</div>`)
+// document.write(`<div class="text" >GPS:  ${geO.lat} | ${geO.lng}</div>`)
+//
+// document.write(`</div>`)
