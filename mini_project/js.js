179a0bf8ed30d054e3730cbd802d9607d1168243
juraantoniv <juraantoniv@gmail.com>
2022-08-24 11:00:44 +0300

    ok

--- mini_project/js.js
+++ mini_project/js.js
@@ -1,21 +1,39 @@
 let box = document.createElement('div')
 box.classList.add('box_all')
 
-fetch('https://jsonplaceholder.typicode.com/users')
+fetch('https://jsonplaceholder.typicode.com/users/')
     .then((response) => response.json())
     .then(users => {
         for (const user of users) {
+            localStorage.setItem('id',JSON.stringify(users))
             let div = document.createElement('div');
-            div.innerText = `${user.id} ${user.name} ${user.username}`;
             div.classList.add('user_all')
-            document.body.appendChild(div);
-            let div1 = document.createElement('div')
-            div1.classList.add('user_name')
-            div1.innerText = `${user.email}`
-            div.appendChild(div1)
-            box.appendChild(div)
-        }
 
+            let name =document.createElement('div')
+            name.innerText = `${user.id} ${user.name} ${user.username}`
+            div.appendChild(name)
+
+
+            let tel = document.createElement('h5')
+            tel.innerText=`tel. ${user.phone}`
+            div.appendChild(tel)
+
+            let web = document.createElement('h4')
+            web.innerText=` ${user.website}`
+            div.appendChild(web)
+
+
+
+            let email = document.createElement('div')
+            email.innerText = `${user.email}`
+            div.appendChild(email)
+                box.appendChild(div)
+
+            let url = document.createElement('a')
+            url.href =`user-details.html?id=${user.id}`
+            url.innerText= 'Ifo of user'
+            div.appendChild(url)
+        }
     });
 
 document.body.appendChild(box)
